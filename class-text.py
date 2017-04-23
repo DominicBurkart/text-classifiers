ec5ca285248724a62a31a38cf30c9c80072fecce
Dominic Burkart <1dominicb@gmail.com>
2017-04-23 12:22:46 +0200

    added csv input/output functionality for ease of use

--- class-text.py
+++ class-text.py
@@ -6,9 +6,7 @@ USAGE:
 1.) make sure that the current directory is the folder that contains this
 file and the other bayes encoding files in this suite.
 
-2.) enter the command "python3 encode-equal-word-features-bayes.py path/to/input.tsv",
-where path/to/input.tsv is replaced with the path to the text to be encoded.
-If the filepath includes spaces, you need to put it in quotes (path/to/input.tsv vs "path/to/in put.tsv").
+2.) enter the command 'python3 encode-equal-word-features-bayes.py "text to be encoded"',
 
 Make sure that the TSV with the text to be encoded has a "message" column – that's
 what this program uses.
@@ -42,29 +40,13 @@ def classify(text):
     Returns the relative probability that a given string would appear on
     Hillary Clinton's twitter (versus on Donald Trump's).
     """
-    try:
-        p = cl.prob_classify(getDict(text))
-        return p.prob('C')
-    except TypeError:
-        print("One line of input could not be interpreted as text. Excluding.")
-        return "NA"
+    p = cl.prob_classify(getDict(text))
+    return p.prob('C')
 
-is_tsv = True;
 
 #read in the input tsv
 try:
-    if (sys.argv[1].lower().endswith("tsv")):
-        data = read_table(sys.argv[1])
-    elif (sys.argv[1].lower().endswith("csv")):
-        is_tsv = False;
-        data = read_csv(sys.argv[1])
-    else:
-        print("The input filetype could not be inferred by the file name. attempting to parse as TSV.")
-        data = read_table(sys.argv[1])
-except OSError:
-    print("The given input does not point to a valid file. Please try again! Input: "+sys.argv[1])
-    print("Quitting.")
-    sys.exit()
+    text = sys.argv[1]
 except IndexError:
     print("No input was passed; quitting. Come back with data for me to eat!")
     sys.exit()
@@ -81,14 +63,4 @@ with open(file_path, 'rb') as f_in:
 cl = pickle.loads(bytes_in)
 
 #calculate probabilities
-texts = list(data.message)
-probs = []
-for t in texts:
-    probs.append(classify(t))
-
-#append probabilities to input tsv
-data['probabilityClinton'] = probs
-if is_tsv:
-    data.to_csv(sys.argv[1], index=False, sep="\t", doublequote=False, escapechar="\\")
-else:
-    data.to_csv(sys.argv[1], index=False, escapechar="\\")
+print(classify(text))
